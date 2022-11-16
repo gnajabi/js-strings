70d7c4f75b7b23fe0b8b1995f25fe855e278f79a
gnajabi <gnajabi123@gmail.com>
2022-11-16 11:17:04 +0300

    string methods

--- strings.js
+++ strings.js
@@ -6,3 +6,8 @@ function sliceString(puppet, start, end){
     let part = puppet.slice(start, end)
     document.getElementById("paragraph3").innerHTML=part
 }
+
+function subString(puppet, start, end){
+    let sub = puppet.substring(start, end)
+    document.getElementById("paragraph4").innerHTML=sub
+}
